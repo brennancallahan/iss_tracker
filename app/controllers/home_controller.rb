597554a9beb337c918f4233c35bab698fd99617a
Brennan Callahan <bren.cal@gmail.com>
2021-03-31 17:44:46 -0400

    added search results parsing logic, handed down data to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,10 +32,24 @@ class HomeController < ApplicationController
 
   def search
     search_query = params[:search]
-    @results = find_lat_and_long(search_query)
+    @results = get_search_results(search_query)
+    @location_data = @results["display_name"]
+    @lat = @results["lat"]
+    @lon = @results["lon"]
+    @iss_pass_data = find_iss_passes(@lat, @lon)
+
     respond_to do |format|
       format.js
     end
+  end
+
+  def get_search_results(search_query)
+    base_path = "https://nominatim.openstreetmap.org/search?q="
+    query = "#{search_query}"
+    return_format = "json"
+    url = base_path + "'#{query}'" + "&format=" + return_format
+    responses = HTTParty.get(url)
+    responses.first
   end
 
   def find_lat_and_long(search_query)
@@ -53,7 +67,13 @@ class HomeController < ApplicationController
 
   def find_iss_passes(lat, lon)
     url = "http://api.open-notify.org/iss-pass.json?lat=#{lat}&lon=#{lon}&n=3"
-    responses = HTTParty.get(url)
+    response = HTTParty.get(url)
+    @duration = response["response"].first["duration"]
+    @risetime = time_parse(response["response"].first["risetime"])
+  end
+
+  def time_parse(time)
+    Time.at(time).to_datetime
   end
 
 end
