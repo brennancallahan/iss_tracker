22bc886384cb19c1974102cd879b5f8f99aca7a1
Brennan Callahan <bren.cal@gmail.com>
2021-03-31 17:13:34 -0400

    built basic lat/lon search function, built partial and ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,11 +31,29 @@ class HomeController < ApplicationController
   end
 
   def search
-    @results = params[:search]
-    
+    search_query = params[:search]
+    @results = find_lat_and_long(search_query)
     respond_to do |format|
       format.js
     end
+  end
+
+  def find_lat_and_long(search_query)
+    base_path = "https://nominatim.openstreetmap.org/search?q="
+    query = "#{search_query}"
+    return_format = "json"
+    url = base_path + "'#{query}'" + "&format=" + return_format
+    responses = HTTParty.get(url)
+    @response = responses.first
+    lat = responses.first["lat"]
+    lon = responses.first["lon"]
+
+    find_iss_passes(lat, lon)
+  end
+
+  def find_iss_passes(lat, lon)
+    url = "http://api.open-notify.org/iss-pass.json?lat=#{lat}&lon=#{lon}&n=3"
+    responses = HTTParty.get(url)
   end
 
 end
